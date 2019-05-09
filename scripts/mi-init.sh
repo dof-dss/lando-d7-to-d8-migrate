11c3ea412fab03f318ed0d128a3178430cb7f893
Johan Gant <johan.gant@gmail.com>
2019-05-09 14:30:33 +0100

    Fixed and enhancements for the migrate init script
    - Checks for presence of private files path with correct variable
    - Checks for local settings, creates file with config settings if not present.
    - Sets working directory for composer, drush and drupal console as required.
    - Fix table reference in tidy up command.

--- scripts/mi-init.sh
+++ scripts/mi-init.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
+set -x
 
-PRIVATE_FILES_PATH = 'app/private'
+PRIVATE_FILES_PATH='/app/private'
+LOCAL_SETTINGS='/app/drupal8/web/sites/default/settings.local.php'
 
 if [ ! -d "$PRIVATE_FILES_PATH" ]; then
-  mkdir PRIVATE_FILES_PATH
+  mkdir $PRIVATE_FILES_PATH
 fi
 
-// Append the private files and legacy database connection to settings.php
-cat config/drupal_settings >> app/web/sites/default/settings.php
+if [ ! -f "$LOCAL_SETTINGS" ]; then
+  # Append the private files and legacy database connection to settings.php
+  echo "<?php\n" > $LOCAL_SETTINGS
+  cat /app/config/drupal_settings >> $LOCAL_SETTINGS
+fi
 
-// Add common migrate contrib modules
-cd app && composer require --dev drupal/migrate_plus drupal/migrate_tools drupal/migrate_upgrade
+# Composer file sits under the D8 root??
+cd /app/drupal8
+# Add common migrate contrib modules
+composer require --dev drupal/migrate_plus drupal/migrate_tools drupal/migrate_upgrade
 
-// Fix Column 'title' cannot be null issues.
-drush sqlq --database=drupal7db "UPDATE node SET node.title = '<none>' WHERE title = '' or title IS NULL; UPDATE node SET node_revision.title = '<none>' WHERE title = '' or title IS NULL;"
+# Fix Column 'title' cannot be null issues.
+drush sql:query --database=drupal7db "UPDATE node SET node.title = '<none>' WHERE title = '' or title IS NULL; UPDATE node_revision SET title = '<none>' WHERE title = '' or title IS NULL;"
 
-// Fix issue with zero status redirect imports to Drupal 8 (Cred to Jaime Contreras)
-drush sqlq --database=drupal7db "UPDATE redirect SET status_code=301 WHERE status_code=0;UPDATE redirect SET status_code=301 WHERE status_code IS NULL;"
+# Fix issue with zero status redirect imports to Drupal 8 (Cred to Jaime Contreras)
+drush sql:query --database=drupal7db "UPDATE redirect SET status_code=301 WHERE status_code=0;UPDATE redirect SET status_code=301 WHERE status_code IS NULL;"
+
+drush en -y migrate migrate_plus miigrate_tools migrate_upgrade
