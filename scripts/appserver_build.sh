debf33bbc8ffacd0bf5bd121625f2c4a22b1d198
Steve Cousins <me@stevecousins.co.uk>
2019-05-24 13:36:07 +0100

    D8NID-44 ~ Adding local development config. Added existing config detection.

--- scripts/appserver_build.sh
+++ scripts/appserver_build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 DRUPAL_SETTINGS_FILE=/app/drupal8/web/sites/default/settings.php
+DRUPAL_LOCAL_SETTINGS_FILE=/app/drupal8/web/sites/default/settings.local.php
 
 if [ ! -d "/app/exports" ]; then
   echo "Creating export directories"
@@ -12,21 +13,31 @@ if [ ! -d "/app/drupal8" ]; then
   echo "Downloading Drupal"
   git clone git@svegit01.thestables.net:dss/nidirect-d8.git /app/drupal8/
   composer -d/app/drupal8 install
+  echo "Building Drupal files"
   composer -d/app/drupal8 drupal:scaffold
   composer -d/app/drupal8 run-script post-install-cmd
 fi
 
+# Create Drupal private file directory above web root.
 if [ ! -d "/app/drupal8/private" ]; then
   echo "Creating private Drupal files directory"
   mkdir -p /app/drupal8/private
 fi
 
-if test -f "$DRUPAL_SETTINGS_FILE"; then
-  echo "Updating Drupal Settings File"
+# Copy example.settings.local.php for local development settings.
+if test -f "$DRUPAL_LOCAL_SETTINGS_FILE"; then
+  echo "Creating settings.local.php"
+  cp /app/drupal8/web/sites/example.settings.local.php /app/drupal8/web/sites/default/settings.local.php
+fi
+
+# Append our lando specific config to the end of settings.php. 
+if ! grep -q "D7 to D8 Migrate settings" "$DRUPAL_SETTINGS_FILE"; then
+  echo "Updating settings.php"
   cat /app/config/drupal_settings >> $DRUPAL_SETTINGS_FILE
 fi
 
 # Put PHPUnit config in place.
 if [ -f "/app/config/phpunit.lando.xml" ]; then
+  echo "Copying PHPUnit config to Drupal webroot"
   ln -sf /app/config/phpunit.lando.xml /app/drupal8/web/core/phpunit.xml
 fi
