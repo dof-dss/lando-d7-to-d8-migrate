9ff0b4473935d284eed4d330e4c6d1cc8c4f1909
Johan Gant <johan.gant@gmail.com>
2019-10-15 08:45:37 +0100

    Bugfix to correct a bash command and escaping of quotes

--- scripts/appserver_build.sh
+++ scripts/appserver_build.sh
@@ -89,11 +89,11 @@ chmod -w $DRUPAL_ROOT/sites/default
 # Set Simple test variables and put PHPUnit config in place.
 if [ ! -f "${DRUPAL_ROOT}/core/phpunit.xml" ]; then
   echo "Adding localised PHPUnit config to Drupal webroot"
-  $DRUPAL_ROOT/core/phpunit.xml.dist $DRUPAL_ROOT/core/phpunit.xml
+  cp $DRUPAL_ROOT/core/phpunit.xml.dist $DRUPAL_ROOT/core/phpunit.xml
   # Fix bootstrap path
   sed -i -e "s|bootstrap=\"tests/bootstrap.php\"|bootstrap=\"${DRUPAL_ROOT}/core/tests/bootstrap.php\"|g" $DRUPAL_ROOT/core/phpunit.xml
   # Inject database params for kernel tests.
-  sed -i -e "s|name=\"SIMPLETEST_DB\" value=\"\"|name=\"SIMPLETEST_DB\" value="${DB_DRIVER}://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}"|g" $DRUPAL_ROOT/core/phpunit.xml
+  sed -i -e "s|name=\"SIMPLETEST_DB\" value=\"\"|name=\"SIMPLETEST_DB\" value=\"${DB_DRIVER}://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}\"|g" $DRUPAL_ROOT/core/phpunit.xml
   # Uncomment option to switch off Symfony deprecatons helper (we use drupal-check for this).
   sed -i -e "s|<!-- <env name=\"SYMFONY_DEPRECATIONS_HELPER\" value=\"disabled\"/> -->|<env name=\"SYMFONY_DEPRECATIONS_HELPER\" value=\"disabled\"/>|g" $DRUPAL_ROOT/core/phpunit.xml
   # Set the base URL for kernel tests.
